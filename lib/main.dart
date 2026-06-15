3ab39bff40011ea21d4b297d1ee4bb3eec7f3767
AsifHossainCSE <asifhossain5145@gmai.com>
2026-06-15 19:28:35 +0600

    Add Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:instagram_flutter/reponsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/reponsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/reponsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utilis/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
